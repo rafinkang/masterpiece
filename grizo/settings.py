f1f309f5a3fc66e3f0a0069474927b3944c6b4af
Taeuk Kang <rkdxodnr07@gmail.com>
2020-12-10 16:45:56 +0900

    db setting

--- grizo/settings.py
+++ grizo/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'grizo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'masterpiece',
+        'USER': 'scott',
+        'PASSWORD': 'tigertiger',
+        'HOST': 'jhta.cpg6w8n0aifr.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
