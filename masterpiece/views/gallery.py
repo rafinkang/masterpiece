26bc3c9995b62b0bf25b7e47c2ce305c2d1ecbce
kimminsu <kimsubbae113@naver.com>
2020-12-17 14:09:56 +0900

    color_gallery na om

---
+++ masterpiece/views/gallery.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from masterpiece.models import *
+from masterpiece.example_class.forms import post_frm
+from masterpiece.example_class.DbConn import *
+
+# Create your views here.
+
+# 색상갤러리 페이지 이동
+def color_gallery(request):
+    return render(request, 'gallery/color_gallery.html')
