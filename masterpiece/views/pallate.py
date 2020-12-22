4f7f50e6eabda36183948d916d96711bb8bfd32b
Taeuk Kang <rkdxodnr07@gmail.com>
2020-12-22 15:43:33 +0900

    base54 to image

--- masterpiece/views/pallate.py
+++ masterpiece/views/pallate.py
@@ -1,16 +1,17 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from masterpiece.classes.CycleganLoadWeight import CycleganLoadWeight
 from django.core.files.base import ContentFile
 import datetime
 import base64
+from PIL import Image
+from io import BytesIO
 
 def pallate(request):
     return render(request, 'pallate/pallate.html')
 
-def ch_style(request):
-    return render(request, 'pallate/ch_style.html')
-
+# ch_style
 def temp_img_upload(request):
     dataURI = request.POST.dict()['dataURI']
     temp_img_path = 'masterpiece/images/tmp'
@@ -32,3 +33,21 @@ def change_masterpiece(request):
 
     return 'ok'
 
+# color_pick
+def color_pick(request):
+    request_dict = request.POST.dict()
+    dataURI = request_dict['dataURI']
+    img = base64_to_image(dataURI)
+    print(img)
+    
+    
+    return HttpResponse('hihi')
+
+def base64_decode(base64_str):
+    img_str = base64_str.split(';base64,')[1]
+    imgdata = base64.b64decode(img_str)
+    return imgdata
+
+def base64_to_image(base64_str):
+    img_str = base64_str.split(';base64,')[1]
+    return Image.open(BytesIO(base64.b64decode(img_str)))
