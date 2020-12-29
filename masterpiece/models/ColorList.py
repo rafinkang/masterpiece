70bc23fec20ae8e4d538ea5c8f7b432a58b1dd1e
Taeuk Kang <rkdxodnr07@gmail.com>
2020-12-29 17:35:07 +0900

    컬러 갤러리

--- masterpiece/models/ColorList.py
+++ masterpiece/models/ColorList.py
@@ -45,3 +45,66 @@ class ColorList():
         """
         return self.db.execute(sql)
         
+        
+    def list_filter(self, color_type = None, season_type = None, cw_type = None, cp_type = None, value_type = None, limit_start = 0, limit_end = 100):
+        sql = "select * from color_list"
+        where = ""
+        if color_type or season_type or cw_type or cp_type or value_type:
+            where += " where "
+            where_value = ""
+            
+            if color_type: 
+                if len(where_value) > 0 : where_value += " and "
+                where_value += "color in ("
+                for index, val in enumerate(color_type.split(',')):
+                    if index == 0:
+                        where_value += "'"+val+"'"
+                    else:
+                        where_value += ", '"+val+"'"
+                where_value += ")"
+                
+            if season_type: 
+                if len(where_value) > 0 : where_value += " and "
+                where_value += "season in ("
+                for index, val in enumerate(season_type.split(',')):
+                    if index == 0:
+                        where_value += "'"+val+"'"
+                    else:
+                        where_value += ", '"+val+"'"
+                where_value += ")"
+                
+            if cw_type: 
+                if len(where_value) > 0 : where_value += " and "
+                where_value += "cw in ("
+                for index, val in enumerate(cw_type.split(',')):
+                    if index == 0:
+                        where_value += "'"+val+"'"
+                    else:
+                        where_value += ", '"+val+"'"
+                where_value += ")"
+                
+            if cp_type: 
+                if len(where_value) > 0 : where_value += " and "
+                where_value += "cp in ("
+                for index, val in enumerate(cp_type.split(',')):
+                    if index == 0:
+                        where_value += "'"+val+"'"
+                    else:
+                        where_value += ", '"+val+"'"
+                where_value += ")"
+                
+            if value_type: 
+                if len(where_value) > 0 : where_value += " and "
+                where_value += "value in ("
+                for index, val in enumerate(value_type.split(',')):
+                    if index == 0:
+                        where_value += "'"+val+"'"
+                    else:
+                        where_value += ", '"+val+"'"
+                where_value += ")"
+                
+                
+            where = where + where_value
+            
+        sql = sql + where + f" limit {limit_start}, {limit_end};"
+        return self.db.select(sql)
