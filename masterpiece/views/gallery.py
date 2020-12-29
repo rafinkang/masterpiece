0d555c48c5b3d71b5ba113de6456a195f4637397
Taeuk Kang <rkdxodnr07@gmail.com>
2020-12-29 16:06:15 +0900

    컬러갤러리폴더구조변경

--- masterpiece/views/gallery.py
+++ masterpiece/views/gallery.py
@@ -8,11 +8,11 @@ from masterpiece.models.GallaryList import GallaryList
 
 # 색상갤러리 페이지 이동
 def color_gallery(request):
-    return render(request, 'gallery/color_gallery.html')
+    return render(request, 'gallery/color_gallery/color_gallery.html')
 
 # 색상갤러리 디테일 페이지 이동
 def color_gallery_detail(request):
-    return render(request, 'gallery/color_gallery_detail.html')
+    return render(request, 'gallery/color_gallery/color_gallery_detail.html')
 
 # 이미지 리스트 이동
 def image_list(request):
