cd473d7b55c6e160fd2866170d3c14ae808ed3c0
Taeuk Kang <rkdxodnr07@gmail.com>
2020-12-30 14:12:48 +0900

    temp파일 삭제

--- test/coloraision/test.py
+++
@@ -1,9 +0,0 @@
-from RainbowSpoid import ColorTrade
-
-image_path1 = "./test/images/suflower.jpg"
-image_path2 = "./test/images/sunflower.jpg"
-
-CT = ColorTrade(image_path1,image_path2)
-CT.plt_img()
-# print(RS1.get_color_ranking())
-# print(RS2.get_color_ranking())
